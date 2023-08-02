28fcd2d7539b0227073ccb97faf9ba444d0542cd
James Simeon <jamessimeon60@gmail.com>
2023-08-02 00:02:14 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+}
+
+function prependCat(name) {
+    var newArray = cats.slice();
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat() {
+    var newArray = cats.slice();
+    newArray.pop(name);
+    return newArray;
+}
+
+function removeFirstCat() {
+    var newArray = cats.slice();
+    newArray.shift(name);
+    return newArray;
+}
